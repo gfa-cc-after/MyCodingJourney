59cb78a6a86e3cccb282ba7a55e44baf31573e8a
Rene Vajda <129347078+ReneVajda1@users.noreply.github.com>
2024-06-14 15:09:28 +0200

    Delete ProjectPhase/Maven-TEST-Login-Page directory

--- ProjectPhase/Maven-TEST-Login-Page/src/main/java/MavenLoginPageTest/Maven/TEST/Login/Page/repositories/UserRepository.java
+++
@@ -1,9 +0,0 @@
-package MavenLoginPageTest.Maven.TEST.Login.Page.repositories;
-
-import MavenLoginPageTest.Maven.TEST.Login.Page.models.User;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface UserRepository extends JpaRepository<User, Long> {
-}
