1e42b702e58479c0df14fd01e89b31edadcca354
ReneVajda1 <rene.vajda@yahoo.com>
2024-06-14 15:36:49 +0200

    feat: adding UserServices class

---
+++ ProjectPhase/MavenTestProjectLogin/src/main/java/MavenTestProjectLogin/MavenTestProjectLogin/services/UserService.java
@@ -0,0 +1,16 @@
+package MavenTestProjectLogin.MavenTestProjectLogin.services;
+
+import MavenTestProjectLogin.MavenTestProjectLogin.interfaces.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserService {
+
+    private final UserRepository userRepository;
+
+    @Autowired
+    public UserService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+}
